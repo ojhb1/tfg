2c4cfd3c09dd3ae814ab70775833f4f304cf7864
ojhb1 <ojhb1@gcloud.ua.es>
2024-12-13 21:20:21 +0100

    Guardado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 
 //PAGINAS PRINCIPALES
 import {IndexComponent} from './paginas/index/index.component';
+import { AppLayoutComponent } from './layout/app.layout.component';
 
 export const routes: Routes = [
     {
         //LAYOUT PRINCIPAL
         path: '', 
-        component: IndexComponent
+        component:AppLayoutComponent,
+        children:[
+           {path:'', component: IndexComponent}
+        ]
+        
         }
 ];
